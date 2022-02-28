2ee0fbba24ada33ed475d584026055d3ae26ca19
mizan <dewan.mizanur911@gmail.com>
2022-03-01 04:56:00 +0600

    firebase installed and configured

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDxdspo0hVgvgu7wgRtRaP1_7QtAKRMFqQ",
+    authDomain: "fashion-club-f5817.firebaseapp.com",
+    projectId: "fashion-club-f5817",
+    storageBucket: "fashion-club-f5817.appspot.com",
+    messagingSenderId: "1059229490700",
+    appId: "1:1059229490700:web:592fa483aa38e652bc1829"
+  };
+
+export default firebaseConfig;
