e402f44efc88a22f13cd6356a97d2605bea0de69
mizan <dewan.mizanur911@gmail.com>
2022-03-01 10:43:14 +0600

    product component design completed

--- src/Pages/Home/Trending/Product/Product.js
+++ src/Pages/Home/Trending/Product/Product.js
@@ -1,11 +1,35 @@
 import React from 'react';
+import { Col, Row } from 'react-bootstrap';
+import Rating from 'react-rating';
 import './Product.css';
+import love from '../../../../images/navigation/wishlist.svg';
 
-const Product = () => {
+const Product = ({product}) => {
+    const {image, title, description, price} = product;
     return (
-        <div>
-            
-        </div>
+        <Row xs={1} md={2} className="border border-1 rounded mt-3 py-3">
+            <Col md={4}>
+                <img src={image} className="trending-product-img" alt="" />
+            </Col>
+            <Col md={8} className="text-start">
+                <h5>{title}</h5>
+                <p>{description}</p>
+                <del className="me-3">${price + (price*20/100)}</del>
+                <b>$ {price}</b>
+                <div className="d-flex align-items-center my-3">
+                <p><small><Rating
+                                initialRating={product.rating?.rate}
+                                readonly
+                                emptySymbol="far fa-star icon-color"
+                                fullSymbol="fas fa-star icon-color"
+                                />({product.rating?.count})
+                    </small>
+                </p>
+                <button className="btn-favorite ms-5"><img src={love} className="favorite-img" alt="" /></button>
+                </div>
+                <button className="btn-general">Add to cart</button>
+            </Col>
+        </Row>
     );
 };
 
--- src/Pages/Home/Trending/Trending/Trending.js
+++ src/Pages/Home/Trending/Trending/Trending.js
@@ -1,14 +1,30 @@
-import React from 'react';
-import { Container } from 'react-bootstrap';
+import React, { useEffect, useState } from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Product from '../Product/Product';
 import './Trending.css';
 
 const Trending = () => {
+    const [products, setProducts] = useState([]);
+    useEffect(()=>{
+        fetch('https://fakestoreapi.com/products?limit=5')
+        .then(res => res.json())
+        .then(data => setProducts(data))
+    },[]);
+    
     return (
         <Container>
-            <div className="d-flex justify-content-between">
+            <div className="d-flex justify-content-between mb-3">
                 <b>Trending Clothes</b>
-                <button>View all</button>
+                <button className="btn-general">View all</button>
             </div>
+            <Row xs={1} className="g-3">
+                {
+                    products.map(product => <Product
+                    key={product.id}
+                    product = {product}
+                    ></Product>)
+                }
+            </Row>
         </Container>
     );
 };
