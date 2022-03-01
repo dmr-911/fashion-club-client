1b7863209573cf18763a9f936f1f76c7f3544a24
mizan <dewan.mizanur911@gmail.com>
2022-03-01 06:46:49 +0600

    navbar completed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,18 @@ import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './Pages/Shared/Navigation/Navigation';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AuthProvider from './Context/AuthProvider';
 
 function App() {
   return (
     <div className="App">
+      <AuthProvider>
       <Router>
         <Navigation></Navigation>
         <Routes>
         </Routes>
       </Router>
+      </AuthProvider>
     </div>
   );
 }
--- src/Pages/Shared/Navigation/Navigation.js
+++ src/Pages/Shared/Navigation/Navigation.js
@@ -1,35 +1,57 @@
 import React from 'react';
 import './Navigation.css';
-import { Badge, Container, Form, FormControl, Nav, Navbar, NavDropdown } from 'react-bootstrap';
-import useAuth from '../../../hooks/useAuth';
+import {Nav, Navbar, NavDropdown } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import useAuth from '../../../hooks/useAuth';
 
 const Navigation = () => {
-    // const {count, user, logOut} = useAuth();
+    const {user, logOut} = useAuth();
     return (
         <Navbar variant="light" expand="lg" className="fixed-top" bg="light">
                 <Navbar.Brand as={Link} to="/"><h2>Fashion Club</h2></Navbar.Brand>
                 <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll" style={{ maxHeight: '100px'}}>
+                <Navbar.Collapse id="navbarScroll">
                 <Nav
-                    className="ms-auto my-2 my-lg-0"
-                    style={{ maxHeight: '100px'}}
-                    navbarScroll
+                    className="mx-auto w-100 my-2 my-lg-0 justify-content-around"
                 >
-                    {/* <Nav.Link as={Link} to={user.email ? "" : "/login"} title={user.email ? user.displayName : "Login"} className="mx-auto">
+                    
+                    <div className="d-flex align-items-center">
+                    <NavDropdown className="mx-2" title="Men" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Men</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown className="mx-2" title="Women" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Women</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    <NavDropdown className="mx-2" title="Kids" id="basic-nav-dropdown">
+                    <NavDropdown.Item href="#action/3.1">Kids</NavDropdown.Item>
+                    <NavDropdown.Divider />
+                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                    </NavDropdown>
+                    <Nav.Link className="mx-2" as={Link} to={user.email ? "" : "/login"} title={user.email ? user.displayName : "Login"} className="mx-auto">
+                        Beauty
+                    </Nav.Link>
+                    </div>
+                    <div className="d-flex align-items-center">
+
+                    <Nav.Link className="mx-2" as={Link} to={user.email ? "" : "/login"} title={user.email ? user.displayName : "Login"} className="mx-auto">
                         <span className="nav-icon nav-account">
                         </span>
-                    </Nav.Link> */}
-                    <Nav.Link as={Link} to="/" className="mx-auto">
+                    </Nav.Link>
+                    <Nav.Link className="mx-2" as={Link} to="/" className="mx-auto">
                         <span className="nav-icon nav-wishlist">
                         </span>
                     </Nav.Link>
-                    <Nav.Link as={Link} to="/bag" className="mx-auto">
+                    <Nav.Link className="mx-2" as={Link} to="/bag" className="mx-auto">
                         <span className="nav-icon nav-cart">
-                            {/* <span className="nav-text">{count <100 ? count : 'NL'}</span> */}
+                            <span className="nav-text">0</span>
                         </span>
                     </Nav.Link>
-                    {/* {user.email && <button className="btn btn-danger" onClick={logOut}>LogOut</button>} */}
+                    {user.email && <button className="btn btn-danger" onClick={logOut}>LogOut</button>}
+                    </div>
                 </Nav>
                 </Navbar.Collapse>
         </Navbar>
