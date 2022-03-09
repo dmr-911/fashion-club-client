13eca3c204dc86cfb78c36c244311775175af123
mizan <dewan.mizanur911@gmail.com>
2022-03-09 12:53:40 +0600

    Hot deals section fully responsive

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -21,8 +21,8 @@ const Home = () => {
             <MiddleBanner></MiddleBanner>
             <Exclusives></Exclusives>
             <Offer></Offer>
-            {/* 
             <HotDeals></HotDeals>
+            {/* 
             <Notify></Notify>
             <Footer></Footer> 
             */}
